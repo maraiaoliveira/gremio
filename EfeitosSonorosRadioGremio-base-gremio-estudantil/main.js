fce70d1448ea6fa4b3c6c631e4ef6ea4a81d6e65
maraiaoliveira <148172311+maraiaoliveira@users.noreply.github.com>
2023-10-23 22:54:23 -0300

    Add files via upload

---
+++ EfeitosSonorosRadioGremio-base-gremio-estudantil/main.js
@@ -0,0 +1,31 @@
+function TocaSomErrado(){
+    document.querySelector("#som_tecla_errado").play();
+}
+function TocaSomErrado(){
+    document.querySelector("#som_tecla_errado").play();
+}
+function TocaSomErrado(){
+    document.querySelector("#som_tecla_errado").play();
+}
+function TocaSomErrado(){
+    document.querySelector("#som_tecla_errado").play();
+}
+function TocaSomErrado(){
+    document.querySelector("#som_tecla_errado").play();
+}
+function TocaSomErrado(){
+    document.querySelector("#som_tecla_errado").play();
+}
+function TocaSomErrado(){
+    document.querySelector("#som_tecla_errado").play();
+}
+function TocaSomErrado(){
+    document.querySelector("#som_tecla_errado").play();
+}
+function TocaSomErrado(){
+    document.querySelector("#som_tecla_errado").play();
+}
+
+
+
+
